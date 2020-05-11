44e73ba90046b803bbc3afc324bc28c03ded3023
mzora <mzora.soul@gmail.com>
2020-05-11 15:42:48 +0200

    Add project files.

---
+++ ASP.Net_ReverseString/Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ASP.Net_ReverseString.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
+    <link href="Content/style.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container col-8">
+            <div class="row justify-content-center" style="margin-top:20vh">
+                <div class="form-group d-inline">
+                    <asp:TextBox ID="TextBox1" runat="server" CssClass="form-control" placeholder="Text to Reverse"></asp:TextBox>
+                </div>
+                <div class="d-inline" style="margin-left:10px">
+                    <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" CssClass="btn btn-info" />
+                </div>
+            </div>
+
+            <div class="form-group">
+                <asp:GridView ID="GridView1" runat="server" CssClass="table table-bordered"></asp:GridView>
+            </div>
+        </div>
+    </form>
+
+    <script src="Scripts/bootstrap.min.js"></script>
+    <script src="Scripts/jquery-3.5.1.min.js"></script>
+    <script src="Scripts/popper.min.js"></script>
+</body>
+</html>
---
+++ ASP.Net_ReverseString/Default.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP.Net_ReverseString
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            GridView1.Visible = true;
+            createNewRow();
+        }
+
+        public string Reverse(string s)
+        {
+            char[] cArray = s.ToCharArray();
+            string reverse = String.Empty;
+
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                reverse += cArray[i];
+            }
+            return reverse;
+        }
+
+        public void createNewRow()
+        {
+            
+            string inputString = TextBox1.Text;
+            string reverseString = Reverse(inputString);
+
+            DataTable mytable = new DataTable();
+
+            if (ViewState["Row"] != null)
+            {
+                mytable = (DataTable)ViewState["Row"];
+
+                if (mytable.Rows.Count > 0)
+                {
+                    DataRow dr = mytable.NewRow();
+                    dr["Input String"] = inputString;
+                    dr["Reverse String"] = reverseString;
+                    mytable.Rows.Add(dr);
+                    ViewState["Row"] = mytable;
+                    
+                    GridView1.DataSource = ViewState["Row"];
+                    GridView1.DataBind();
+                }
+            }
+            else
+            {
+                mytable.Columns.Add("Input String", typeof(string));
+                mytable.Columns.Add("Reverse String", typeof(string));
+
+                DataRow dr1 = mytable.NewRow();
+                dr1["Input String"] = inputString;
+                dr1["Reverse String"] = reverseString;
+                mytable.Rows.Add(dr1);
+
+                ViewState["Row"] = mytable;
+                GridView1.DataSource = ViewState["Row"];
+                GridView1.DataBind();
+            }
+        }
+    }
+}
